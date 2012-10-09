1b465963399ee8a6173de680cb7aa9e300c26cf5
Mike Bevz <me@mikebevz.com>
2012-10-10 00:51:25 +0200

    Added wrapper for Symfony2

---
+++ lib/tmhOAuth/tmhOAuth.php
@@ -0,0 +1,7 @@
+<?php
+# vendor/tmhOAuth/lib/tmhOAuth/tmhOAuth.php
+
+require_once __DIR__.'/../../tmhOAuth.php';
+
+class tmhOAuth_tmhOAuth extends tmhOAuth {
+}
---
+++ lib/tmhOAuth/tmhUtilities.php
@@ -0,0 +1,7 @@
+<?php
+# vendor/tmhOAuth/lib/tmhOAuth/tmhUtilities.php
+
+require_once __DIR__.'/../../tmhUtilities.php';
+
+class tmhUtilities_tmhUtilities extends tmhUtilities {
+}
